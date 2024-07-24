a37806ae07edd59405093d15c8b416cf28daa536
adhitizki <adhitizki@adhitizki>
2024-07-24 21:44:08 +0700

    update app.py: variable joblib

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from MLProject.config.configuration import ConfigurationManager
 from MLProject.components.predict import Predict
 from MLProject import logger
 
+import joblib
+
 try:
     config = ConfigurationManager()
     predict_config = config.get_prediction_config()
+    data = joblib.load(predict_config.input_valid_path)
     predict = Predict(config=predict_config)
     result = predict.run(data)
 except Exception as e:
