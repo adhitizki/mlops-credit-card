db83692398771fe018b31a3cdd86662ef428cdc5
adhitizki <adhitizki@adhitizki>
2024-07-25 21:09:19 +0700

    update app.py: added fastapi

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
+import os
+import logging
+
 from MLProject.config.configuration import ConfigurationManager
 from MLProject.components.predict import Predict
-from MLProject import logger
 
-import joblib
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
 
-try:
-    config = ConfigurationManager()
+os.putenv("LANG", "en_US.UTF-8")
+os.putenv("LC_ALL", "en_US.UTF-8")
+
+app = FastAPI()
+config = ConfigurationManager()
+
+class Item(BaseModel):
+    columns: List[str]
+    data : List[List[float]]
+
+@app.get("/")
+def read_root():
+    return {"message": "Up!"}
+
+@app.post("/predict")
+def predict(item: Item):
+    logging.info("Access data")
+    columns = item.columns
+    data = item.data
+
+    logging.info("Load configuration.")
+    data = pd.DataFrame(data, columns=columns)
     predict_config = config.get_prediction_config()
-    data = joblib.load(predict_config.input_valid_path)
     predict = Predict(config=predict_config)
+    
+    logging.info("Make prediction.")
     result = predict.run(data)
-except Exception as e:
-    logger.error(e)
-    raise e
+    
+    return result
